50ead85e3fc8d304124cb05e4327c0f9d54ef600
Shion <ehdrhelr@gmail.com>
2020-12-07 12:59:03 +0900

    feat : add moving right

--- Main.java
+++ Main.java
@@ -5,19 +5,42 @@ public class Main {
     String word = "";
     String number = "";
     String direction = "";
+
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         
         Main main = new Main();
         main.inputWordNumberDirection(sc);
+        main.moveCharToRight();
     }
 
     public void inputWordNumberDirection(Scanner sc) {
+        System.out.print("> ");
         String input = sc.nextLine();
         String[] WordNumberDirection = input.split(" ");
         word = WordNumberDirection[0];
         number = WordNumberDirection[1];
         direction = WordNumberDirection[2];
+    }
+
+    public void moveCharToRight() {
+        String[] eachCharOfWord = word.split("");
+        String[] newWord = new String[word.length()];
+        for (int i = 0; i < word.length(); i++) {
+            int targetIndex = getTargetIndex(i);
+            newWord[targetIndex] = eachCharOfWord[i];
+        }
+        for (String str : newWord) {
+            System.out.print(str);
+        }
+    }
+
+    public int getTargetIndex(int currentIndex) {
+        int targetIndex = currentIndex + Integer.valueOf(number);
+        if (targetIndex >= word.length()) {
+            targetIndex -= word.length();
+        }
+        return targetIndex;
     }
 
 
