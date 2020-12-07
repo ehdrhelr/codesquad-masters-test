abcbaefc479844b331c2988413306d715850a6ec
Shion <ehdrhelr@gmail.com>
2020-12-07 15:32:40 +0900

    impl initial print

--- Main.java
+++ Main.java
@@ -2,74 +2,21 @@ import java.util.*;
 
 public class Main {
 
-    String word = "";
-    String number = "";
-    String direction = "";
-
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         Main main = new Main();
-        main.start(sc);
-    }
-
-    public void start(Scanner sc) {
-        while (true) {
-            inputWordNumberDirection(sc);
-            System.out.println(moveChar());
+        
+        String[][] cube = {
+            {"R", "R", "W"},
+            {"G", "C", "W"},
+            {"G", "B", "B"}
+        };
+        
+        for (int i = 0; i < cube.length; i++) {
+            for (int j = 0; j < cube[i].length; j++) {
+                System.out.print(cube[i][j] + " ");
+            } 
+            System.out.println();
         }
-    }
-
-    public void inputWordNumberDirection(Scanner sc) {
-        System.out.print("> ");
-        String input = sc.nextLine();
-        String[] WordNumberDirection = input.split(" ");
-        word = WordNumberDirection[0];
-        number = WordNumberDirection[1];
-        direction = WordNumberDirection[2];
-    }
-
-    public String moveChar() {
-        String[] eachCharOfWord = word.split("");
-        String[] eachCharOfNewWord = new String[word.length()];
-        String newWord = "";
-        for (int i = 0; i < word.length(); i++) {
-            eachCharOfNewWord[getTargetIndex(i)] = eachCharOfWord[i];
-        }
-        for (String eachChar : eachCharOfNewWord) {
-            newWord += eachChar;
-        }
-        return newWord;
-    }
-
-    public int getTargetIndex(int currentIndex) {
-        if (direction.equalsIgnoreCase("R") && number.startsWith("-")) {
-            return pushLeft(currentIndex);
-        }
-        if (direction.equalsIgnoreCase("L") && number.startsWith("-")) {
-            return pushRight(currentIndex);
-        }
-        if (direction.equalsIgnoreCase("R")) {
-            return pushRight(currentIndex);
-        }
-        if (direction.equalsIgnoreCase("L")) {
-            return pushLeft(currentIndex);
-        }
-        return -1;
-    }
-
-    public int pushRight(int currentIndex) {
-        int targetIndex = currentIndex + Math.abs(Integer.valueOf(number));
-        while (targetIndex >= word.length()) {
-            targetIndex -= word.length();
-        }
-        return targetIndex;
-    }
-
-    public int pushLeft(int currentIndex) {
-        int targetIndex = currentIndex - Math.abs(Integer.valueOf(number));
-        while (targetIndex < 0) {
-            targetIndex += word.length();
-        }
-        return targetIndex;
     }
 }
